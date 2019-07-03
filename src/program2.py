1b28b3864bc2608715983653b018ce188af415d6
alexey <alexey@semmle.com>
2019-07-03 09:32:49 +0100

    Add more alerts

--- src/program2.py
+++ src/program2.py
@@ -5,3 +5,14 @@ class X(object):
 class Y(object,X):
     def __init__(self):
         print("Y")
+
+print(50)
+
+class A(object):
+    def __init__(self):
+        print("X")
+
+class B(object,X):
+    def __init__(self):
+        print("Y")
+
